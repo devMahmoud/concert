17a7fbc3e9efefa77fb482214a8b5fc3d58d6d0c
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-04-15 22:25:01 +0200

    fix some linters

--- menu.js
+++ menu.js
@@ -3,9 +3,9 @@ const navLinks = document.querySelector('.nav-links');
 const mobileMenu = document.querySelector('.mobile-menu');
 const togleIcon = document.querySelector('#toggle-icon');
 
-toggleBtn.addEventListener ('click', () => {
-    navLinks.classList.toggle('hidden')
-    mobileMenu.classList.toggle('mobile-menu-open');
-    togleIcon.classList.toggle('fa-align-justify');
-    togleIcon.classList.toggle('fa-xmark');
+toggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('hidden')
+  mobileMenu.classList.toggle('mobile-menu-open');
+  togleIcon.classList.toggle('fa-align-justify');
+  togleIcon.classList.toggle('fa-xmark');
 });
--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 const artistGrid = document.querySelector('.artist-cards-container');
 
 const artists = [
-    {
-        name:'Ali Elhagar',
-        job: 'Singer',
-        about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
-        photo: 'img/artists/ali.png',
-    },
-    {
-        name:'Wael Jassar',
-        job: 'Singer',
-        about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
-        photo: 'img/artists/wael.png',
-    },
-    {
-        name:'Samira Said',
-        job: 'Singer',
-        about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
-        photo: 'img/artists/samira.png',
-    },
-    {
-        name:'Marwan Khoury',
-        job: 'Singer',
-        about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
-        photo: 'img/artists/marwan.png',
-    },
-    {
-        name:'Abadi Al Johar',
+  {
+    name: 'Ali Elhagar',
+    job: 'Singer',
+    about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
+    photo: 'img/artists/ali.png',   
+  },
+  {
+    name: 'Wael Jassar',
+    job: 'Singer',
+    about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
+    photo: 'img/artists/wael.png',   
+  },
+  {
+    name: 'Samira Said',
+    job: 'Singer',
+    about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
+    photo: 'img/artists/samira.png',    
+  },
+  {
+    name: 'Marwan Khoury',
+    job: 'Singer',
+    about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
+    photo: 'img/artists/marwan.png',
+  },
+  {
+        name: 'Abadi Al Johar',
         job: 'Singer',
         about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
         photo: 'img/artists/abadi.png',
-    },
-    {
-        name:'Asala',
-        job: 'Singer',
-        about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
-        photo: 'img/artists/asala.png',
-    },
+  },
+  {
+    name: 'Asala',
+    job: 'Singer',
+    about: 'fugit perspiciatis molestias assumenda? Mollitia fuga modi consectetur dolorem cumque quia error reiciendis blanditiis amet, repellendus temporibus accusamus.',
+    photo: 'img/artists/asala.png',
+  },
 ];
 
 const creatArtistCard = (title, jobTitle, info, img) => {
-    const artistCard = document.createElement('div');
-    artistCard.classList.add('artist-card');
-    artistCard.innerHTML = `<img class="artist-photo" src="${img}">
+  const artistCard = document.createElement('div');
+  artistCard.classList.add('artist-card');
+  artistCard.innerHTML = `<img class="artist-photo" src="${img}">
     <div class="artist-card-content">
         <h4 class="artist-name">${title}</h4>
         <p class="artist-job">${jobTitle}</p>
         <p class="artist-about">${info}</p>
     </div>`;
 
-    artistGrid.appendChild(artistCard);
-}
+  artistGrid.appendChild(artistCard);
+};
 
-for (let i = 0; i < artists.length; i += 1){
-    creatArtistCard(artists[i].name, artists[i].job, artists[i].about, artists[i].photo)
+for (let i = 0; i < artists.length; i += 1) {
+  creatArtistCard(artists[i].name, artists[i].job, artists[i].about, artists[i].photo);
 }
